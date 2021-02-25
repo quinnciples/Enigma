2401d091dd3cd3ef4ab8db5e819e588c054df936
quinnciples <49799428+quinnciples@users.noreply.github.com>
2021-02-24 23:02:51 -0700

    Initial updates.

--- enigma.py
+++ enigma.py
@@ -1,8 +1,17 @@
-# import json
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s.%(msecs)03d - %(levelname)8s - %(filename)s - Function: %(funcName)20s - Line: %(lineno)4s // %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    handlers=[
+                        # logging.FileHandler(filename='log.txt'),
+                        logging.StreamHandler()
+                    ])
+# logging.disable(level=logging.CRITICAL)
+logging.info('Loaded.')
 
 
 class Enigma:
-    """ DOC STRING """
+    """DOC STRING"""
 
     def __init__(self):
         # Reference https://en.wikipedia.org/wiki/Enigma_rotor_details
@@ -37,26 +46,42 @@ class Enigma:
 
         self.reflector = self.rotor_pool['Reflector A']
 
-        self.plugs = ['GDNAPQKOWX',
+        plugs = ['GDNAPQKOWX',
                       'UETVLSJRMZ']
+        self.plugs = {}
+        for char_in, char_out in zip(plugs[0], plugs[1]):
+            self.add_plug_board_connection(key_in=char_in, key_out=char_out)
 
-    def plugBoard(self, c):
-        if c in self.plugs[0]:
-            return self.plugs[1][self.plugs[0].find(c)]
-        elif c in self.plugs[1]:
-            return self.plugs[0][self.plugs[1].find(c)]
+    def add_plug_board_connection(self, key_in: str, key_out: str) -> None:
+        logging.debug(f'Creating plug board connection between {key_in} and {key_out}.')
+        self.plugs[key_in] = key_out
+        self.plugs[key_out] = key_in
+
+    def remove_plug_board_connection(self, key_in: str, key_out: str) -> None:
+        if key_in not in self.plugs or self.plugs.get(key_in, None) != key_out or key_out not in self.plugs or self.plugs.get(key_out, None) != key_in:
+            Exception(f'Connection between {key_in} and {key_out} does not exist on the plugboard.')
         else:
-            return c
+            logging.debug(f'Removing plug board connection between {key_in} and {key_out}.')
+            if key_in in self.plugs:
+                del self.plugs[key_in]
+            if key_out in self.plugs:
+                del self.plugs[key_out]
+
+    def plugBoard(self, key_in):
+        if key_in in self.plugs:
+            logging.debug(f'Translating plug board connection between {key_in} and {self.plugs.get(key_in, key_in)}.')
+        return self.plugs.get(key_in, key_in)
 
     def advanceRotor(self, iterations=1):
 
         for i in range(iterations):
             for r, rotor in reversed(list(enumerate(self.rotors))):
+                logging.debug(f'Advancing rotor {r} from {rotor} to {[rotor[0][1:] + rotor[0][0], rotor[1] + 1]}')
                 rotor_copy = [rotor[0][1:] + rotor[0][0], rotor[1] + 1]
                 self.rotors[r] = rotor_copy
                 if self.rotors[r][1] % 26 == 0:
                     self.rotors[r][1] = 0
-                    # print('Advance next rotor')
+                    logging.debug('Advancing next rotor')
                 else:
                     break
 
@@ -91,17 +116,18 @@ class Enigma:
         return result
 
 
-E = Enigma()
-print('-=' * 40)
-print(E)
-print('-=' * 40)
-original_message = """Pack my box with five dozen liquor jugs Weather report things look slightly cloudy over here Hopefully the weather improves tomorrow"""
-encoded = E.encodeMessage(original_message)
-print('Message in:', original_message, 'Message out:', encoded)
-E.resetMachine()
-message = encoded
-encoded = E.encodeMessage(encoded)
-print('Message in:', message, 'Message out:', encoded, encoded == original_message.strip().upper().replace(' ', 'X'))
-print('-=' * 40)
-print(E)
-print('-=' * 40)
+if __name__ == '__main__':
+    E = Enigma()
+    print('-=' * 40)
+    print(E)
+    print('-=' * 40)
+    original_message = """Pack my box with five dozen liquor jugs Weather report things look slightly cloudy over here Hopefully the weather improves tomorrow"""
+    encoded = E.encodeMessage(original_message)
+    print('Message in:', original_message, 'Message out:', encoded)
+    E.resetMachine()
+    message = encoded
+    encoded = E.encodeMessage(encoded)
+    print('Message in:', message, 'Message out:', encoded, encoded == original_message.strip().upper().replace(' ', 'X'))
+    print('-=' * 40)
+    print(E)
+    print('-=' * 40)
